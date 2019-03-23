c5b0a2bde21de6bc6164679cd256aa332aa21e5b
Kylos Lam <lamky94@gmail.com>
2019-03-24 00:32:40 +0800

    Google Analytic Fixes -fixes issue with the code

--- _static/google_analytics.js
+++ _static/google_analytics.js
@@ -1,9 +1,7 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136825070-1"></script>
-<script>
+src="https://www.googletagmanager.com/gtag/js?id=UA-136825070-1">
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-136825070-1');
-</script>
